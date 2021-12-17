2d39f9b00b2b23075c3a137937cd264e7e98d9d3
tfuzo <tfuzo@hotmail.com>
2021-12-17 17:52:14 +0000

    last

---
+++ src/modules/fztracker/schemas/guns.schema.ts
@@ -0,0 +1,15 @@
+import { Schema } from 'mongoose';
+
+
+
+export const GunsSchema = new Schema({
+  place: { type: String, required: true, unique: true },
+  state: { type: String, required: true },
+  guns: [{}],
+  lastChangeDate: { type: Date, default: Date.now },
+  entitySerial: { type: String },
+  entityDesc: { type: String }
+});
+
+
+GunsSchema.index({ place: 1 });
